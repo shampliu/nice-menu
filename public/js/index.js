3c46ce131a53a498595840e4c12efe638e450130
Chang Liu <shampliu@gmail.com>
2015-08-20 18:25:40 -0700

    ratings

--- public/js/index.js
+++ public/js/index.js
@@ -4,15 +4,28 @@ var main = angular.module('main', []);
 
 function mainCtrl($scope, $http) {
 
+	$scope.decimal = function(num) {
+		if (Math.floor(num) != num) {
+			return true;
+		}
+		return false; 
+	}
+
+	$scope.parseReview = function(string) {
+		return string.split('-')[1];
+	}
+
+	$scope.parseRating = function(string) {
+		var stars = Math.floor(string.split('-')[0]);
+
+		return new Array(stars * 1);
+	}
+
 	$scope.sortProp = 'title.$t';
 
 	var url = "https://spreadsheets.google.com/feeds/list/1n5OTsDtiKS39NUgeDtl_zCYn6YgYwoqT2tLbgwNhHWw/od6/public/values?alt=json";
 	$http.get(url).success(function(data){
-		console.log(data.feed.entry);
 		$scope.places = data.feed.entry;
-
-
-	    // console.log(JSON.parse(data));
 	});
 
 	$scope.truncate = function(string) {
